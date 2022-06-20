706ac04eba0085cbd7f0689d05d38a61caa92d34
LucaSpennato <luca.spennato@gmail.com>
2022-06-20 19:05:58 +0200

    milestone 3

--- js/script.js
+++ js/script.js
@@ -67,17 +67,75 @@ let givenImages = [
 ];
 
 let imgContainer;
+let imgArray = [];
 
  for (let index = 0; index < givenImages.length; index++) {
-    console.log(givenImages[index]);
-
+    
     imgContainer = document.createElement('img');
     imgContainer.classList.add('img-fluid');
     imgContainer.setAttribute('src', givenImages[index]);
     carouselWrapper.append(imgContainer);
+
+    imgArray.push(imgContainer);
     
 }
 
+console.log(imgArray);
 
+// prendiamo il wrapper contentente le immagini
+console.log(carouselWrapper);
 
+// salviamo in una variabile i figli, creando tipo un array
+const imgList = imgArray;
+// console.log(imgList);
+
+// funzionando come un array, diciamo ad una variabile di avere un attributo numerico che sappiamo 
+// si collegerà il numero index dell'array
+let activeImg = 0;
+console.log(imgList[activeImg]);
+
+imgList[activeImg].classList.add('active');
+
+const prevBtn = document.getElementById('prev_btn');
+const nextBtn = document.getElementById('next_btn');
+
+// next button
+nextBtn.addEventListener('click', function(){
+
+    //ora al click, aggiungiamo e togliiamo le classi per visualizzare in base al 
+    // numero al quale abbiamo assegnato ad activeImg
+    // praticamente stiamo prendendo l'elemento0 nella ''lista dell'array''
+    imgList[activeImg].classList.remove('active');
     
+    // aggiungiamo un +1 all'elemento che ci permette di selezionare l'index nella lista dell'array
+    // semplicemente con ++ per dirgli di aumentare di uno e passare al prossimo
+    activeImg++;
+
+    // cosa stiamo facendo qua? Stiam dicendo che, se il numero di activeImg, 
+    // arriva alla lunghezza del numero di elementi presenti nella lista 'array', 
+    // allora riparti dal numero 0, dall'elemento 0, perchè altrimenti, premendo si andrà avanti
+    // superando così' il numero di elementi presenti e semplicemente non vedremo nulla!!
+    if (activeImg === imgList.length){
+        activeImg = 1;
+    }
+
+    // aggiungiamo la classe attivo per vedere la successiva
+    imgList[activeImg].classList.add('active');
+    
+
+})
+
+
+prevBtn.addEventListener('click', function(){
+    imgList[activeImg].classList.remove('active');
+    
+    activeImg--;
+    
+    if (activeImg < imgList.length[0]){
+        activeImg = imgList[5];
+    }
+    
+    imgList[activeImg].classList.add('active');
+    
+    
+})
