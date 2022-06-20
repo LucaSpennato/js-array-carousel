508ca2f29f5fc784497e2c9e4f2f942185ef0fdb
LucaSpennato <luca.spennato@gmail.com>
2022-06-20 16:10:20 +0200

    nxtbtn remove d-none, add d-block

--- js/script.js
+++ js/script.js
@@ -1,15 +1,25 @@
 const carouselWrapper = document.getElementById('carousel_wrapper');
-console.log(carouselWrapper);
+// console.log(carouselWrapper);
 
 const imgList = carouselWrapper.children;
-console.log(imgList);
+// console.log(imgList);
+
+let activeImg = 0;
+console.log(imgList[activeImg]);
 
 const prevBtn = document.getElementById('prev_btn');
 const nextBtn = document.getElementById('next_btn');
 
-
 // next button
-prevBtn.addEventListener('click', function(){
+nextBtn.addEventListener('click', function(){
 
+    imgList[activeImg].classList.remove('d-none');
+    imgList[activeImg].classList.add('d-block');
 
 })
+
+
+for (let index = 0; index < imgList.length; index++) {
+    // console.log(imgList[index]);
+    
+}
