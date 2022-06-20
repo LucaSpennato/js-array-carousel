9eae1214dc6c1d050335b3ab627aa32ff93aae68
LucaSpennato <luca.spennato@gmail.com>
2022-06-20 15:54:46 +0200

    static- button, wrapper, img var creat

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const carouselWrapper = document.getElementById('carousel_wrapper');
+console.log(carouselWrapper);
+
+const imgList = carouselWrapper.children;
+console.log(imgList);
+
+const prevBtn = document.getElementById('prev_btn');
+const nextBtn = document.getElementById('next_btn');
+
+
+// next button
+prevBtn.addEventListener('click', function(){
+
+
+})
