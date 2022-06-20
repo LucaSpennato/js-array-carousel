40004bd2939340c05fd12512aa14836f261ddd20
LucaSpennato <luca.spennato@gmail.com>
2022-06-20 16:40:09 +0200

    comments add, css add

--- js/script.js
+++ js/script.js
@@ -1,11 +1,17 @@
+// prendiamo il wrapper contentente le immagini
 const carouselWrapper = document.getElementById('carousel_wrapper');
 // console.log(carouselWrapper);
 
+// salviamo in una variabile i figli, creando tipo un array
 const imgList = carouselWrapper.children;
 // console.log(imgList);
 
+// funzionando come un array, diciamo ad una variabile di avere un attributo numerico che sappiamo 
+// si collegerà il numero index dell'array
 let activeImg = 0;
 console.log(imgList[activeImg]);
+
+imgList[activeImg].classList.add('active');
 
 const prevBtn = document.getElementById('prev_btn');
 const nextBtn = document.getElementById('next_btn');
@@ -13,8 +19,18 @@ const nextBtn = document.getElementById('next_btn');
 // next button
 nextBtn.addEventListener('click', function(){
 
-    imgList[activeImg].classList.remove('d-none');
-    imgList[activeImg].classList.add('d-block');
+    //ora al click, aggiungiamo e togliiamo le classi per visualizzare in base al 
+    // numero al quale abbiamo assegnato ad activeImg
+    // praticamente stiamo prendendo l'elemento0 nella ''lista dell'array''
+    imgList[activeImg].classList.remove('active');
+    
+    // aggiungiamo un +1 all'elemento che ci permette di selezionare l'index nella lista dell'array
+    // semplicemente con ++ per dirgli di aumentare di uno e passare al prossimo
+    activeImg++;
+
+    // aggiungiamo la classe attivo per vedere la successiva
+    imgList[activeImg].classList.add('active');
+    
 
 })
 
